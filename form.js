35134d1333369c115ee93a237fab94bbcbefad48
Samuel Andrade <samuelandrade.spa@gmail.com>
2024-12-26 23:53:34 -0300

    auterações

--- form.js
+++ form.js
@@ -1,7 +1,7 @@
 function validateForm() {
-    const email = document.getElementById ('email').value;
+    const email = document.getElementById('email').value;
     if (!email.includes('@')) {
-        alert('Por favor, insira um e-mail válido.')
+        alert('Por favor, insira um e-mail válido.');
         return false;
     }
     return true;
