d4d7064b654a8fdcce65320b456043046dbd8a6c
Samuel Andrade <samuelandrade.spa@gmail.com>
2025-01-01 14:34:47 -0300

    Required no Form

--- form.js
+++ form.js
@@ -1,18 +1,42 @@
 function validateForm() {
+    const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
-    if (!email.includes('@')) {
-        alert('Por favor, insira um e-mail válido.');
-        return false;
-    }
-    return true;
-}
+    const surname = document.getElementById('surname').value;
+    const radio = document.getElementById('radio').value;
+    const check = document.getElementById('check').value;
+    let isValid = true;
 
-function validateForm() {
-    const usernameInput = document.getElementById ('username');
-    const minLength = 4;
+    document.getElementById('username').setAttribute('required',"");
+    document.getElementById('surname').setAttribute('required', "");
+    document.getElementById('email').setAttribute('required', "");
+    document.getElementById('radio').setAttribute('required', "");
+    document.getElementById('check').setAttribute('required', "");
+        
+    if (username.length < 3) {
+        console.log('Nome deve ter mais de 3 caracteres');
+        isValid = false;
+    } else {
+        console.log('Nome Váido');
+    }
 
-    if (!usernameInput.value.length < minLength) {
-        alert('O nome de usuário deve ter pelo menos $ {4}');
-        event.preventDefault();
+    if (surname.length < 3) {
+        console.log('Sobrenome deve haver mais de 3 caracteres')
+        isValid = false;
+    } else {
+        console.log('Sobrenome válido')
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailPattern.test(email)) {
+        console.log('Email inválido');
+        isValid = false;
+    } else {
+        console.log('Email válido');
+    }
+
+    if(isValid) {
+        console.log('Fomulário válido, pronto para ser enviado')
     }
 }
+ 
+alert('site em construcao')
